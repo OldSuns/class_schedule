e6a5b3e650ec46c9cf0e0a860aaad19f332d8c85
OldSun <34978144+OldSuns@users.noreply.github.com>
2026-04-02 11:07:10 +0800

    version: Update to 2.0.1

--- src/config/constants.js
+++ src/config/constants.js
@@ -44,7 +44,7 @@ export const ELECTIVE_OPTIONS = [
 ];
 
 // 当前应用版本（用于更新检查）
-export const APP_VERSION = "1.9.15";
+export const APP_VERSION = "2.0.1";
 
 // Gitee Releases
 export const GITHUB_RELEASES_URL =
